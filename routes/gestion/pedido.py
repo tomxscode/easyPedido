b4cbc08f0877e477607a9e479b966e6851742957
Tomás Barros <104880585+tomxscode@users.noreply.github.com>
2023-11-17 22:44:37 -0300

    Mejora en la vista de pedidos

--- routes/gestion/pedido.py
+++ routes/gestion/pedido.py
@@ -2,12 +2,15 @@ from flask import Blueprint, redirect
 from models import db
 from models.Pedido import Pedido
 from models.PedidoProducto import PedidoProducto
+from models.ProductoMenu import ProductoMenu
 from models.Mesa import Mesa
 from flask_login import login_required
 from forms.forms import PedidoForm
 from flask import url_for
 from flask import flash
 from flask import render_template
+from flask import request
+from flask import jsonify
 
 pedido = Blueprint('pedido', __name__)
 
@@ -45,4 +48,34 @@ def crear():
 def ver(id):
   pedido = Pedido.query.get(id)
   productos_pedido = PedidoProducto.query.filter_by(pedido=id).all()
-  return render_template('admin/pedido.html', pedido=pedido, productos=productos_pedido)
+  def obtener_nombre_producto(id_producto):
+    producto = ProductoMenu.query.get(id_producto)
+    return producto.nombre if producto else 'Producto no encontrado'
+  return render_template('admin/pedido.html', pedido=pedido, productos=productos_pedido, obtener_nombre_producto=obtener_nombre_producto)
+
+@pedido.route('/api/agregar_producto_pedido/', methods=['POST'])
+@login_required
+def agregar_producto():
+  # Obtener desde un JSON la info para trabajarla
+  cantidad = request.json['cantidad']
+  producto = request.json['producto']
+  detalle = request.json['detalle']
+  pedido_id = request.json['pedido']
+  estado = 1
+  
+  # Comprobar que el producto existe
+  producto_encontrado = ProductoMenu.query.get(producto)
+  if not producto_encontrado:
+    return jsonify({'success': False, 'message': 'El producto no ha sido encontrado'})
+  # Comprobar que el pedido existe
+  pedido_encontrado = Pedido.query.get(pedido_id)
+  if not pedido_encontrado:
+    return jsonify({'success': False, 'message': 'El pedido no ha sido encontrado'})
+  # Si los demas campos no han sido enviados, entonces devuelve error 400
+  if not cantidad:
+    return jsonify({'success': False, 'message': 'Los campos cantidad y detalle son obligatorios'})
+
+  producto_pedido_nuevo = PedidoProducto(cantidad=cantidad, producto=producto, detalle=detalle, pedido=pedido_id, estado=estado)
+  db.session.add(producto_pedido_nuevo)
+  db.session.commit()
+  return jsonify({'success': True, 'message': 'Producto agregado correctamente'})
--- static/js/producto_orden.js
+++ static/js/producto_orden.js
@@ -24,7 +24,7 @@ busquedaInput.addEventListener('input', () => {
   coincidencias_productos.forEach(producto => {
     if (producto.nombre.toLowerCase().includes(busquedaInput.value.toLowerCase())) {
       coindicencias.innerHTML += `
-      <li class="list-group-item d-flex justify-content-between align-items-center">
+      <li class="list-group-item d-flex justify-content-between align-items-center mt-2">
         ${producto.nombre}
         <span class="badge badge-primary badge-pill">$${producto.precio}</span>
         <button class="btn btn-primary" onclick="agregarProducto(${producto.id})">Agregar</button>
@@ -33,3 +33,68 @@ busquedaInput.addEventListener('input', () => {
     }
   });
 });
+
+const agregarProducto = (id) => {
+  Swal.fire({
+    title: 'Agregar producto al pedido',
+    // Input "cantidad"
+    html: `
+      <input id="cantidad" type="number" class="form-control" placeholder="Cantidad" min="1" max="100" value="1" required>
+      <input id="detalle" type="text" class="form-control" placeholder="Detalle" required>
+    `,
+    showCancelButton: true,
+    confirmButtonText: 'Agregar',
+    cancelButtonText: 'Cancelar',
+    focusConfirm: false,
+    preConfirm: () => {
+      const cantidad = Swal.getPopup().querySelector('#cantidad').value;
+      const detalle = Swal.getPopup().querySelector('#detalle').value;
+      if (!cantidad) {
+        Swal.showValidationMessage(`El campo cantidad es obligatorio`);
+      }
+      return { cantidad: cantidad, detalle: detalle };
+    }
+  })
+    .then((result) => {
+      if (result.isConfirmed) {
+        const cantidad = result.value.cantidad;
+        const detalle = result.value.detalle;
+        fetch(`${SITIO}api/agregar_producto_pedido/`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            producto: id,
+            cantidad: cantidad,
+            detalle: detalle,
+            pedido: PEDIDO_ID
+          })
+        })
+          .then(response => response.json())
+          .then(data => {
+            if (data.success) {
+              Swal.fire({
+                icon: 'success',
+                title: 'Producto agregado al pedido',
+                showConfirmButton: false,
+                timer: 1500
+              })
+                .then(() => {
+                  location.reload();
+                });
+            } else {
+              Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: data.message
+              })
+            }
+          })
+          .catch(error => {
+            console.error(error);
+          });
+
+      }
+    })
+}
